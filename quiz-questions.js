e6b91bf1f713178b13340d1ef40fd44a5da141f2
Shahd Mustafa <99287558+shahdmu34@users.noreply.github.com>
2023-10-22 02:16:04 -0400

    - commit -shahd

--- quiz-questions.js
+++ quiz-questions.js
@@ -2,106 +2,132 @@ const quiz_Questions = [
     {
         question: "You need a weapon what do you choose:",
         answer: [
-            {text:"Your fists", extrovert: true},
-            {text:"Archery", introvert: true },
-            {text:"Sword" ,extrovert: false },
-            {text:"Knife",  introvert: false },
+            {text:"Your fists", extrovert: 2},
+            {text:"Archery", introvert: 2 },
+            {text:"Sword" ,extrovert: 1 },
+            {text:"Knife",  introvert: 1 },
            
         ]
     },
     {
         question: "Choose a power: ",
         answer: [
-            {text:"Invisibility " , introvert: true },
-            {text:"Healing", extrovert: false },
-            {text:"Flying " , introvert: false  },
-            {text:"Super strength", extrovert: true},
+            {text:"Invisibility " , introvert: 2 },
+            {text:"Healing", extrovert: 1 },
+            {text:"Flying " , introvert: 1  },
+            {text:"Super strength", extrovert: 2},
            
         ]
     },
     {
         question: "You have to pick a road. Do you ",
         answer: [
-            {text:"Go the direct path you already know but long" , routine: true},
-            {text:"Grab a boat and go", spontaneous: true  },
-            {text:"Woods is dangerous but shorter", spontaneous:false },
-            {text:"Go on a road with a map", routine: false},
+            {text:"Go the direct path you already know but long" , routine: 2},
+            {text:"Grab a boat and go", spontaneous: 2  },
+            {text:"Woods is dangerous but shorter", spontaneous:1 },
+            {text:"Go on a road with a map", routine: 1},
            
         ]
     },
     {
         question: "A monster shows up. How should you defeat him",
         answer: [
-            {text:"Fight him with your weapon", spontaneous: true },
-            {text:"Hide", routine: true },
-            {text:"Throw something at it" , spontaneous: false },
-            {text:"Talk it out" , routine: false},
+            {text:"Fight him with your weapon", spontaneous: 2 },
+            {text:"Hide", routine: 2 },
+            {text:"Throw something at it" , spontaneous: 1 },
+            {text:"Talk it out" , routine: 1},
            
         ]
     },
     {
         question: "You  find people that are lost in the forest",
         answer: [
-            {text:"Do you lead them" , Leader: true},
-            {text:"Suggest where they can find help" , follower: false},
-            {text:"Do you give them directions", Leader: false },
-            {text:"Don’t help they seem suspicious", follower: true},
+            {text:"Do you lead them" , Leader: 2},
+            {text:"Suggest where they can find help" , follower: 1},
+            {text:"Do you give them directions", Leader: 1 },
+            {text:"Don’t help they seem suspicious", follower: 2},
            
         ]
     },
     {
         question: "choose your companion",
         answer: [
-            {text:"Fast Cat" , Leader:  false},
-            {text:"Vision Bird" ,Leader: true },
-            {text:"Buff Ants" , follower: false },
-            {text:"Good bite Dog" , follower: true },
+            {text:"Fast Cat" , Leader:  1},
+            {text:"Vision Bird" ,Leader: 2 },
+            {text:"Buff Ants" , follower: 1 },
+            {text:"Good bite Dog" , follower: 2 },
            
         ]
     },
     {
         question: "A wall you need to cross with a guard",
         answer: [
-            {text:"Dig a hole and go under", creative: false },
-            {text:"Build a ladder with things you find around you" , creative: true },
-            {text:"Look for any hidden entrance", analytical:true },
-            {text:"Bargain with the guard" , analytical: false},
+            {text:"Dig a hole and go under", creative: 1 },
+            {text:"Build a ladder with things you find around you" , creative: 2 },
+            {text:"Look for any hidden entrance", analytical: 2 },
+            {text:"Bargain with the guard" , analytical: 1},
            
         ]
     },
     {
         question: "You meet a turtle what do you do?",
         answer: [
-            {text:"Say hi(but judge the turtle)" , analytical: true },
-            {text:"Ignore the turtle" , analytical: false},
-            {text:"Ask the turtle a question" , creative: false },
-            {text:"Befriend the turtle" , creative: true},
+            {text:"Say hi(but judge the turtle)" , analytical: 2 },
+            {text:"Ignore the turtle" , analytical: 1},
+            {text:"Ask the turtle a question" , creative: 1 },
+            {text:"Befriend the turtle" , creative: 2},
            
         ]
     },
     {
         question: "Someone asks for money. Do you",
         answer: [
-            {text:"give it to them" , purpose: true},
-            {text:"Give a little" , purpose: false },
-            {text:"trade it for something" , money:false },
-            {text:"Apologize", money: true },
+            {text:"give it to them" , purpose: 2},
+            {text:"Give a little" , purpose: 1 },
+            {text:"trade it for something" , money:1 },
+            {text:"Apologize", money: 1 },
            
         ]
     },
     {
         question: "What is your adventure:",
         answer: [
-            {text:"Chasing a bounty", money: true },
-            {text:"Become the strongest in the realm" , money:false},
-            {text:"Save your bestie(turtle)" ,purpose: true},
-            {text:"Explore the world and learn" , purpose: false },
+            {text:"Chasing a bounty", money: 2 },
+            {text:"Become the strongest in the realm" , money:1},
+            {text:"Save your bestie(turtle)" ,purpose: 2},
+            {text:"Explore the world and learn" , purpose: 1 },
            
         ]
     }
     
 
 ];
+/*
+const careerOptions = [
+    {
+        career_array: ["introver","spontaneous","follower", "money", "analytical"],
+        career: "Computer Science"
+    },
+    {
+        career_array: ["extrovert", "spontaneous" , "follower" , "purpose", "analytical"],
+        career: "HealthCare"
+    },
+    {
+        career_array: ["extrovert", "spontaneous", "money" , "creative"],
+        career: "Business"
+    },{
+        career_array: ["creative" , "routine", "leader", "purpose","analytical" ],
+        career: "Psych"
+    },
+    {
+        career_array: ["introvert", "routine", "follower", "purpose", "analytical"  ],
+        career: "Psych"
+    },{
+        career_array: ["extrovert", "spontaneous" , "leader", "purpose" , "creative"  ],
+        career: "Psych"
+    }
+]
+*/
 //pulling in html
 const question_elem = document.getElementById("question");
 const userAnswer = document.getElementById("answerBtns");
@@ -126,13 +152,27 @@ const healthCare = ["extrovert", "spontaneous" , "follower" , "purpose", "analyt
 const business = ["extrovert", "spontaneous", "money" , "creative"];
 const psych = ["creative" , "routine", "leader", "purpose","analytical" ];
 const law = ["introvert", "routine", "follower", "purpose", "analytical"  ];
-const art = ["extrovert", "spontaneous" , "leader", "purpose" , "creative"  ]
+const art = ["extrovert", "spontaneous" , "leader", "purpose" , "creative"  ];
+const RealEstate = ["extrovert", "spontaneous", "follower", "leader", "analytical"];
 
 //start quiz
 function beginQuiz(){
     promptInxdex = 0;
-    use_Score = 0;
-    nextBtn.innerHTML = "Next";
+    user_score_array = [];
+    answer_promt = [];
+    
+    extrovert_score =0;
+    introvert_score = 0;
+    routine_score = 0;
+    spontaneous_score = 0;
+    leader_score = 0;
+    follower_score = 0;
+    purpose_score = 0;
+    money_score = 0;
+    creative_score = 0;
+    analytical_score = 0;
+    
+   nextBtn.innerHTML = "Next";
     displayPrompts();
 
     
@@ -148,7 +188,8 @@ function displayPrompts(){
         btn.innerHTML = answers.text;
         btn.classList.add("btn");
         userAnswer.appendChild(btn);
-        
+        //answer_promt.push(quiz_Questions.qId);
+      
         if(answers.extrovert){
             btn.dataset.extrovert = answers.extrovert;
         }else if(answers.introvert){
@@ -182,33 +223,32 @@ function resetPrompts(){
     }
 }
 
+
+
 function promptResults(e){
     const btnSelect = e.target;
-    const user_extrovert = btnSelect.dataset.extrovert ==="true";
-    const user_introvert = btnSelect.dataset.extrovert ==="true";
-    const user_routine = btnSelect.dataset.extrovert ==="true";
-    const user_spontaneous = btnSelect.dataset.extrovert ==="true";
-    const user_leader = btnSelect.dataset.extrovert ==="true";
-    const user_follower = btnSelect.dataset.extrovert ==="true";
-    const user_purpose = btnSelect.dataset.extrovert ==="true";
-    const user_money = btnSelect.dataset.extrovert ==="true";
-    const user_creative = btnSelect.dataset.extrovert ==="true";
-    const user_analytical = btnSelect.dataset.extrovert ==="true";
-
+   
     //if extrovert was selected
     if(user_extrovert){
         btnSelect.classList.add("veryextrovert");
         extrovert_score = extrovert_score + 2;
+
+        
     }else if(!user_extrovert){
         btnSelect.classList.add("extrovert");
         extrovert_score = extrovert_score + 1;
+
     }else if(user_introvert){
         btnSelect.classList.add("veryintrovert");
         introvert_score = introvert_score + 2;
+       
+
     }else if(!user_introvert){
         btnSelect.classList.add("introvert");
         introvert_score = introvert_score + 1;
-    }else if(user_routine){
+
+    }
+    if(user_routine){
         btnSelect.classList.add("veryroutine");
         routine_score = routine_score + 2;
     }else if(!user_routine){
@@ -220,7 +260,8 @@ function promptResults(e){
     }else if(!user_spontaneous){
         btnSelect.classList.add("Spontaneous");
         spontaneous_score = spontaneous_score + 1;
-    }else if(user_leader){
+    }
+     if(user_leader){
         btnSelect.classList.add("veryLeader");
         leader_score = leader_score + 2;
     }else if(!user_leader){
@@ -257,9 +298,7 @@ function promptResults(e){
         btnSelect.classList.add("analytical");
         analytical_score = analytical_score + 1;
     }
-    Array.from(userAnswer.children).forEach( btn => {
-        //disable user clicking on different btns
-    });
+    
     nextBtn.style.display = "block";
 }
 
@@ -295,45 +334,23 @@ function careerType(){
         user_score_array.push("analytical");
 
     }
+    console.log(user_score_array);
     return user_score_array;
 }
+
 
 function user_Result(){
     resetPrompts();
 
     const user_score_array = careerType();
 
-    //not working
-    let result = "";
-    //compare the two arrays to get answer!!
-    if(arrayCompare(user_score_array , computerScience)){
-        result = "Computer Sience"; 
-    }else if(arrayCompare(user_score_array , healthCare)){
-        result = "healthcare";
-    }else if(arrayCompare(user_score_array , business)){
-        result = "business";
-    }else if(arrayCompare(user_score_array , psych)){
-        result = "psych";
-    }else if(arrayCompare(user_score_array , law)){
-        result = "law";
-    }
   
     //output the answer
-    question_elem.innerHTML = `end of quiz! ${result}`;
+    question_elem.innerHTML = `end of quiz! ${user_score_array}`;
     nextBtn.innerHTML = `Back TO Home Page!`;
    // nextBtn.style.display = "block";
 }
-function arrayCompare(userArray, careerArray){
-    if(userArray.length !== careerArray.length){
-        return false;
-    }
-    for(let i = 0; i < userArray.length; i++){
-        if(userArray[i] !== careerArray[i]){
-            return false;
-        }
-    }
-    return true;
-}
+
 function nextBtn_funct(){
     promptInxdex++;
     if(promptInxdex < quiz_Questions.length){
